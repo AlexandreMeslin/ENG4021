77339d9f100a863d0586f7f73c91314b4ad2ba80
AlexandreMeslin <27902382+AlexandreMeslin@users.noreply.github.com>
2026-05-29 12:49:31 +0000

    Adiciona funcionalidade de autenticação com páginas de login e rotas necessárias

--- MeuProjeto-2026-1-33B/MeuSite/MeuSite/settings.py
+++ MeuProjeto-2026-1-33B/MeuSite/MeuSite/settings.py
@@ -117,3 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# ---------
+# Código que eu precisei incluir
+# ---------
+
+LOGOUT_REDIRECT_URL = '/accounts/login/'  # Para onde vai após logout
+LOGIN_URL = '/accounts/login/'       # URL de login (padrão)
+# ATENÇÃO!!! Troque o valor da variável abaixo para que ela seja uma das rotas
+# válidas no seu arquivo MeuSite/curriculo/urls.py
+LOGIN_REDIRECT_URL = '/'   # Para onde vai após login
