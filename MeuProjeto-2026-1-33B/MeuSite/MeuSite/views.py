2255ebd6f5ceec3bc29e6f871c94aa694c573df7
AlexandreMeslin <27902382+AlexandreMeslin@users.noreply.github.com>
2026-05-29 21:27:44 +0000

    Aula do dia 2026-05-29

--- MeuProjeto-2026-1-33B/MeuSite/MeuSite/views.py
+++ MeuProjeto-2026-1-33B/MeuSite/MeuSite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     '''
@@ -6,3 +7,12 @@ def home(request):
     Renders the home.html template.
     '''
     return render(request, 'MeuSite/home.html')
+
+@login_required
+def secreta(request):
+    '''
+    View para renderizar a página secreta que é o template secreta.html
+    Esse view é básico para renderizar páginas
+    Apenas tenho que trocar o nome da função e o nome do template
+    '''
+    return render(request, 'MeuSite/secreta.html')
